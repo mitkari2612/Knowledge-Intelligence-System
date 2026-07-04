1dafcb1f54b8e1c3f6cfecea938380312ea5279e
mitkari2612 <ajaymitkari2612@gmail.com>
2026-07-05 01:25:51 +0530

    Added project files

---
+++ app/services/llm_service.py
@@ -0,0 +1,36 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationalBufferMemory
+from config import Config
+
+
+
+class LLMService:
+    def __init__(self,vectore_store):
+        self.llm =ChatOpenAI(
+            temperature=0.7,
+            model_name="gpt-3.5-turbo"
+            openai_api_key=Config.OPENAI_API_KEY
+        )
+        
+        self.memory =ConversationBufferMemory(
+            memory_key ="chat_history",
+            return_message=True
+        )
+        
+        self.chain =ConversationalRetrievalChain(
+            llm =self.llm,
+            retriever=vectore_store.vectorestore.as_retriever(),
+            memory=self.memory
+        )
+        
+        def get_response(self,query):
+            try:
+                response = self.chain({"question": query})
+                return response['answer']
+            except Exception as e:
+                print(f"Error getting LLM response : {e}")
+                return "I encountered an error processing your request. "
+                
+            
+        
